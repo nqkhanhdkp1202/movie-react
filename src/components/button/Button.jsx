70970a6eaf536153173ab8e38ce8c5d8ba82b430
nqkhanh2003 <nqkhanh2003@gmail.com>
2023-02-21 12:48:58 +0700

    feat: add btn & modal component, header scroll

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,26 @@
+import './button.scss';
+import React from 'react'
+import PropTypes from "prop-types";
+
+const Button = props => {
+    return (
+        <button
+            className={`btn ${props.className}`}
+            children={props.children}
+            onClick={props.onClick ? () => props.onClick() : null
+            }/>
+    )
+};
+
+const OutlineButton = props => {
+    return <Button className={`btn-outline ${props.className}`}
+                   children={props.children}
+                   onClick={props.onClick ? () => props.onClick() : null}
+    />
+}
+
+Button.propTypes = {
+    onClick: PropTypes.func
+}
+
+export default Button;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
+import './header.scss'
+import {Link, useLocation} from "react-router-dom";
 
 import logo from '../../assets/tmovie.png'
 
+const headerNav = [
+    {
+        display: 'Home',
+        path: '/',
+    },
+    {
+        display: 'Movies',
+        path: '/movie',
+    },
+    {
+        display: 'TV Series',
+        path: '/tv',
+    }
+];
+
+
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    const {pathname} = useLocation();
+    const headerRef = useRef(null);
+
+    const active = headerNav.findIndex(e => e.path === pathname);
+
+    useEffect(() => {
+        const shrinkHeader = () => {
+            if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+                headerRef.current.classList.add('shrink');
+            } else {
+                headerRef.current.classList.remove('shrink');
+            }
+        }
+        window.addEventListener('scroll', shrinkHeader);
+        return {}
+    }, []);
+    
+    return (
+        <div ref={headerRef} className="header">
+            <div className="header__wrap container">
+                <div className="logo">
+                    <img src={logo} alt=""/>
+                    <Link to="/" children="tMovies"/>
+                </div>
+                <ul className="header__nav">
+                    {
+                        headerNav.map((e, i) => (
+                            <li key={i} className={`${i === active ? 'active' : ''}`}>
+                                <Link to={e.path} children={e.display}/>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default Header
