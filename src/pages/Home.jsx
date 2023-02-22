8ccf8c512003cc4656b42717211d43ce7b77ed76
nqkhanh2003 <nqkhanh2003@gmail.com>
2023-02-22 18:41:13 +0700

    styling: styled content in hero slide

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
 import HeroSlide from "../components/hero-slide/HeroSlide.jsx";
+import {Link} from "react-router-dom";
+import {OutlineButton} from "../components/button/Button.jsx";
 
 const Home = () => {
     return (
         <div>
             <HeroSlide/>
+            <div className="container">
+                <div className="section mb-3">
+                    <h2>
+                        Trending Movies
+                    </h2>
+                    <Link to="/">
+                        <OutlineButton className="small">View more</OutlineButton>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
